551d33716b9aa417078ccc1d6e4b86d4c798d79c
StevenGaughran <73718182+StevenGaughran@users.noreply.github.com>
2022-01-03 12:46:42 -0800

    Add files via upload
    A very simple program that creates a band name using input variables and the print function. Not exactly rocket science, but a good start.

---
+++ DAY_1_Band_Name_Generator.py
@@ -0,0 +1,12 @@
+#The Day 1 project for "Udemy's 100 Days of Code: the Complete Python Pro Bootcamp". Uses variables to store names, string cacatonation to combine them in a print function.
+
+#1. Create a greeting for your program.
+print("Hello, rock star! Let's name your band!")
+#2. Ask the user for the city that they grew up in.
+city = input("What city did you grow up in?\n")
+#3. Ask the user for the name of a pet.
+pet = input("What was the name of your pet?\n")
+#4. Combine the name of their city and pet and show them their band name.
+print("Your band name is " +city +" " +pet +"! Rock on!")
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://replit.com/@appbrewery/band-name-generator-end
