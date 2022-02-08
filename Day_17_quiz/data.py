5dd0bb9727d5f8a3114c91e6a8cb11d8381fc406
StevenGaughran <73718182+StevenGaughran@users.noreply.github.com>
2022-02-08 10:24:13 -0800

    Add files via upload

---
+++ Day_17_quiz/data.py
@@ -0,0 +1,33 @@
+question_data = [
+    {"text": "Linus Torvalds created Linux and Git.", "answer": "True"},
+    {"text": "The programming language &quot;Python&quot; is based off a modified version "
+                 "of &quot;JavaScript&quot;.", "answer": "False"},
+    {"text": "The logo for Snapchat is a Bell.", "answer": "False"},
+    {"text": "Pointers were not used in the original C programming language; they were added later on in C++.",
+     "answer": "False"},
+    {"text": "Ada Lovelace is often considered the first computer programmer.", "answer": "True"},
+    {"text": "&quot;HTML&quot; stands for Hypertext Markup Language.",
+      "answer": "True"},
+    {"text": "Time on Computers is measured via the EPOX System.", "answer": "False"},
+    {"text": "The Windows 7 operating system has six main editions.", "answer": "True"},
+    {"text": "The NVidia GTX 1080 gets its name because it can only render at a 1920x1080 screen resolution.",
+ "answer": "False"},
+    {"text": "Linux was first created as an alternative to Windows XP.", "answer": "False", }
+]
+
+#     [
+#     {"text": "A slug's blood is green.", "answer": "True"},
+#     {"text": "The loudest animal is the African Elephant.", "answer": "False"},
+#     {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
+#     {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
+#     {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, "
+#              "you are free to take it home to eat.", "answer": "True"},
+#     {"text": "In London, UK, if you happen to die in the House of Parliament, "
+#              "you are entitled to a state funeral.", "answer": "False"},
+#     {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
+#     {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
+#     {"text": "Google was originally called 'Backrub'.", "answer": "True"},
+#     {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
+#     {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
+#     {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
+# ]
