263f6b9e5c2e2564e377f432fa403d8c2ed6a0bb
StevenGaughran <73718182+StevenGaughran@users.noreply.github.com>
2022-05-09 09:52:27 -0700

    Delete Axe_HAX directory

--- Axe_HAX/axe_data.py
+++
@@ -1,9 +0,0 @@
-# ~~~~ This handles the API request. ~~~~
-import requests
-
-
-response = requests.get("https://api.opendota.com/api/heroStats")
-response.raise_for_status()
-stats = response.json()
-
-
